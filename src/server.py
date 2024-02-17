7acc572628d7f93263e40e6da87a6875a79670dc
Critiek <jamesvsomeren@gmail.com>
2024-02-17 19:55:11 +1030

    basic echo server/cient on loopback interface

---
+++ src/server.py
@@ -0,0 +1,29 @@
+import socket
+from textual.app import App
+
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
+
+'''
+class MyApp(App):
+    pass
+
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.run()
+'''
