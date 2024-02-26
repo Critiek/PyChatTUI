44bae56eda0930060d4af90123dc3d4fc569dacc
Critiek <jumbra99@gmail.com>
2024-02-26 22:15:51 +1030

    working on simultanious connections

--- src/p2p_client.py
+++ src/p2p_client.py
@@ -1,29 +1,38 @@
 import socket
+import selectors
 from textual.app import App
+
+sel = selectors.DefaultSelector()
 
 def get_local_ipv4():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
 
+
 HOST = get_local_ipv4()
 
-conn, addr = None, None
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 def host_p2p(HOST, PORT):
     s.bind((HOST, PORT))
     s.listen()
+    print(f"Listening on {(HOST, PORT)}")
+    s.setblocking(False)
+    sel.register(s, selectors.EVENT_READ, data=None)
     conn, addr = s.accept()
     return conn, addr
 
+
+conn, addr = None, None
 
 p2p_status = input("Connect or host? ['connect', 'host']\n> ").lower()
 
 if p2p_status == "host":
     PORT = int(input("What port would you like to use?\n> "))
     conn, addr = host_p2p(HOST, PORT)
+    print(f"{addr} has connected")
 
 elif p2p_status == "connect":
     ipv4_port = input("Enter remote address and port formatted as [ipv4:port]\n> ")
