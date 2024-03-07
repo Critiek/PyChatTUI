2c9efd7f7669b1c118e5a7f069554193da9cc303
Critiek <jamesvsomeren@gmail.com>
2024-03-08 00:34:25 +1030

    Add host and connect functionality to main.py

--- src/main.py
+++ src/main.py
@@ -3,3 +3,20 @@ import socket
 client_type = input("Host or connect? ['host', 'connect']\n> ")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if client_type == 'host':
+    s.bind(('127.0.0.1', 12345))    
+    s.listen(5)  # Listen for incoming connections
+    conn, addr = s.accept()
+    print(f"Connected by {addr}")
+    while True:
+        msg = ''
+        msg = conn.recv(1024)
+        msg = msg.decode('utf-8')
+        print(msg)
+        
+elif client_type == 'connect':
+    s.connect(('127.0.0.1', 12345))
+    while True:
+        msg = input("Enter message: ")
+        s.sendall(msg.encode('utf-8'))
