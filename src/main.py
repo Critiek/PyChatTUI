f6d89010a7ff4cfc8f26aad1c6a244377ab118e0
Critiek <jumbra99@gmail.com>
2024-03-08 07:06:59 +1030

    literally just removing some random space

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ client_type = input("Host or connect? ['host', 'connect']\n> ")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if client_type == 'host':
-    s.bind(('127.0.0.1', 12345))    
+    s.bind(('127.0.0.1', 12345))
     s.listen(5)  # Listen for incoming connections
     conn, addr = s.accept()
     print(f"Connected by {addr}")
@@ -14,7 +14,7 @@ if client_type == 'host':
         msg = conn.recv(1024)
         msg = msg.decode('utf-8')
         print(msg)
-        
+
 elif client_type == 'connect':
     s.connect(('127.0.0.1', 12345))
     while True:
